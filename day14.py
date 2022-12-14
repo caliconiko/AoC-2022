61f49c51f280fb0338644ef5c95e8287b75ebb8a
Calico Niko <69907559+photon-niko@users.noreply.github.com>
2022-12-14 14:11:40 +0700

    start 14

---
+++ day14.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+with open("day14test.txt") as f:
+    raw = f.read()
+    lines = raw.splitlines()
+
+def part1():
+    paths = [[(point_raw.) for point_raw in line.split("->")] for line in lines]
+
+part1()
